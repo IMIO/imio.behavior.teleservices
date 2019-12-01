d16fadf778816d122315416da8d2b27911909097
Laurent Lasudry <lasudry@gmail.com>
2019-12-02 00:15:53 +0100

    Fix / simplify procedure viewlet & behavior

--- CONTRIBUTORS.rst
+++ CONTRIBUTORS.rst
@@ -2,3 +2,4 @@ Contributors
 ============
 
 - Christophe Boulanger, christophe.boulanger@imio.be
+- Laurent Lasudry, laurent.lasudry@affinitic.be
--- src/imio/behavior/teleservices/behaviors/ts_procedure.py
+++ src/imio/behavior/teleservices/behaviors/ts_procedure.py
@@ -5,14 +5,7 @@ from plone import schema
 from plone.autoform.interfaces import IFormFieldProvider
 # from plone.dexterity.interfaces import IDexterityContent
 from plone.supermodel import model
-from zope.component import adapter
-from zope.interface import Interface
-from zope.interface import implementer
 from zope.interface import provider
-
-
-class ITsProcedureMarker(Interface):
-    pass
 
 
 @provider(IFormFieldProvider)
@@ -21,14 +14,7 @@ class ITsProcedure(model.Schema):
     """
 
     procedures = schema.Choice(
-            vocabulary="imio.behavior.teleservices.vocabularies.remote_procedures",
-            title=_(u"E-Guichet procedures"),
-            required=False,
-        )
-
-
-@implementer(ITsProcedure)
-@adapter(ITsProcedureMarker)
-class TsProcedure(object):
-    def __init__(self, context):
-        self.context = context
+        vocabulary="imio.behavior.teleservices.vocabularies.remote_procedures",
+        title=_(u"E-Guichet procedures"),
+        required=False,
+    )
