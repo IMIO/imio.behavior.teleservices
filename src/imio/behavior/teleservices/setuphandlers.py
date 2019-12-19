6dea71dbfb28f5e1b95889d404b583d2923b2c35
Christophe Boulanger <christophe.boulanger@imio.be>
2019-12-19 14:22:28 +0100

    Install behavior on procedure type.

--- src/imio/behavior/teleservices/setuphandlers.py
+++ src/imio/behavior/teleservices/setuphandlers.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
+
+from imio.behavior.teleservices.behaviors.ts_procedure import ITsProcedure
+from imio.behavior.teleservices.utils import add_behavior
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
-
     def getNonInstallableProfiles(self):
         """Hide uninstall profile from site-creation and quickinstaller."""
         return [
-            'imio.behavior.teleservices:uninstall',
+            "imio.behavior.teleservices:uninstall",
         ]
 
 
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+    add_behavior("Procedure", ITsProcedure.__identifier__)
 
 
 def uninstall(context):
