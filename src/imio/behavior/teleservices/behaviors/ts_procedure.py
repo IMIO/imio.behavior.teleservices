925a4311f8eae5344560c0b1ce37693885a6df45
Christophe Boulanger <christophe.boulanger@imio.be>
2020-01-16 09:21:13 +0100

    update behavior

--- src/imio/behavior/teleservices/behaviors/ts_procedure.py
+++ src/imio/behavior/teleservices/behaviors/ts_procedure.py
@@ -2,8 +2,9 @@
 
 from imio.behavior.teleservices import _
 from plone import schema
+from plone.autoform import directives
+from imio.behavior.teleservices.widgets import SelectProcedureWidget
 from plone.autoform.interfaces import IFormFieldProvider
-# from plone.dexterity.interfaces import IDexterityContent
 from plone.supermodel import model
 from zope.interface import provider
 
@@ -12,9 +13,19 @@ from zope.interface import provider
 class ITsProcedure(model.Schema):
     """
     """
-
+    directives.widget("procedures", SelectProcedureWidget)
     procedures = schema.Choice(
         vocabulary="imio.behavior.teleservices.vocabularies.remote_procedures",
         title=_(u"E-Guichet procedures"),
         required=False,
+        default=None
     )
+
+# import z3c.form
+# from plone.directives import dexterityclass
+# MyCustomView(dexterity.DisplayForm):
+#    def updateWidgets(self):
+#         super(MyCustomView, self).updateWidgets()
+#         for widget in self.widgets.values():
+#             if not widget.value:
+#                 widget.mode = z3c.form.interfaces.HIDDEN_MODE
